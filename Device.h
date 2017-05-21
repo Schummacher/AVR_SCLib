8a2f4791395d17af0ae2cd8fdf014ccd95488cb4
Schummacher <duschummacher@outlook.com>
2017-05-21 19:38:36 +0800

    Update Device.h

--- Device.h
+++ Device.h
@@ -17,53 +17,6 @@
 #include "USART.h"
 #include "MyLcd.h"
 
-/************************************************************************/
-/* bit control typedef                                                  */
-/************************************************************************/
-struct ass_8{
-	unsigned zero:1;
-	unsigned one:1;
-	unsigned two:1;
-	unsigned three:1;
-	unsigned four:1;
-	unsigned five:1;
-	unsigned six:1;
-	unsigned seven:1;
-};
-
-struct ass_16{
-	unsigned zero:1;
-	unsigned one:1;
-	unsigned two:1;
-	unsigned three:1;
-	unsigned four:1;
-	unsigned five:1;
-	unsigned six:1;
-	unsigned seven:1;
-	unsigned eight:1;
-	unsigned nine:1;
-	unsigned ten:1;
-	unsigned eleven:1;
-	unsigned twelve:1;
-	unsigned thirteen:1;
-	unsigned fourteen:1;
-	unsigned fifteen:1;
-	
-};
-
-union ash_8{
-	uint8_t all;
-	struct ass_8 bit;
-};
-
-union ash_16{
-	uint16_t all;
-	struct ass_16 bit;
-};
-
-typedef union ash_8 bit_8;
-typedef union ash_16 bit_16;
-
 #define LSL(x, y) x=x<<y
 #define LSR(x, y) x=x>>y
 
