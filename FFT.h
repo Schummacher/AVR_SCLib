77a9f5803b119f44fce6a39bb1c8f6433cc70237
Schummacher <duschummacher@outlook.com>
2017-05-29 11:37:09 +0000

    Add files via upload

--- FFT.h
+++ FFT.h
@@ -2,8 +2,8 @@
 
 typedef struct complex //复数类型  
 {
-	float real;       //实部  
-	float imag;       //虚部  
+	float Re;       //实部  
+	float Im;       //虚部  
 }complex;
 
 #define PI 3.1415926535897932384626433832795028841971  
@@ -15,7 +15,8 @@ void c_plus(complex a, complex b, complex *c);//复数加
 void c_mul(complex a, complex b, complex *c);//复数乘  
 void c_sub(complex a, complex b, complex *c); //复数减法  
 void c_div(complex a, complex b, complex *c); //复数除法  
-void fft(int N, complex f[]);//傅立叶变换 输出也存在数组f中  
-void ifft(int N, complex f[]); // 傅里叶逆变换  
-void c_abs(complex f[], float out[], int n);//复数数组取模  
+void FFT(int N, complex f[]);//傅立叶变换 输出也存在数组f中  
+void IFFT(int N, complex f[]); // 傅里叶逆变换  
+void c_abs(complex f[], float out[], int n);//复数数组取模 
+void DFT(complex *data, complex *X, int K);
 ////////////////////////////////////////////
