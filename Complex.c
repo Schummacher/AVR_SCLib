f651248f7af715e992a1548dc8812df986c38aa4
Miku311 <heidazl414@outlook.com>
2017-06-02 16:40:14 +0800

    * and / ；but  re and im should be float..........

--- Complex.c
+++ Complex.c
@@ -21,3 +21,16 @@ cint8_t csub(cint8_t val1, cint8_t val2)
 	val1.im = val1.im - val2.im;
 	return val1;
 }
+cint8_t cmultiply(cint8_t val1, cint8_t val2)
+{
+	val1.re = val1.re * val2.re - val1.im * val2.im;
+	val1.im = val1.im * val2.re + val1.re * val2.im;
+	return val1;
+}
+
+cint8_t cexcept(cint8_t val1, cint8_t val2)
+{
+	val1.re = (val1.re * val2.re + val1.im * val2.im) / (val2.re * val2.re + val2.im * val2.im);
+	val1.im = (val1.im * val2.re - val1.re * val2.im) / (val2.re * val2.re + val2.im * val2.im);
+	return val1;
+}
