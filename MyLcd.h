de26219698c4d300b8eefa0d5699da1d0a897b58
Miku311 <heidazl414@outlook.com>
2017-07-09 14:56:59 +0800

    Update MyLcd.h

--- MyLcd.h
+++ MyLcd.h
@@ -11,11 +11,12 @@
 
 #include "avr/io.h"
 #include "MyLcd.h"
+#define map_value_3 255										//The ADC sampling value from 0 ~ 255 mapping to 0 ~ map_value_3
+#define map_value_4 1023									//The ADC sampling value from 0 ~ 1023 mapping to 0 ~ map_value_4
 
-void my_display(uint8_t x, uint8_t y, uint32_t i);
-void Welcome(void);
+void my_display_3(uint8_t x, uint8_t y, uint32_t i);
+void my_display_4(uint8_t x, uint8_t y, uint32_t i);
 
 extern uint8_t display[3];
-
 
 #endif /* MYLCD_H_ */
