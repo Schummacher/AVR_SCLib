ffed1f4822c091cd8716c08803fe2a90b4c1e2c8
Miku311 <heidazl414@outlook.com>
2017-07-09 17:50:59 +0800

    Annotations

--- ADC.c
+++ ADC.c
@@ -8,6 +8,9 @@
 #include "avr/io.h"
 #include "ADC.h"
 
+
+/******* ADC sampling *******/
+
 uint8_t ADC_Date(uint8_t i)
 {
 	ADMUX = 0x60 + i;			//Vref = AVCC, result of adc right shift
@@ -17,14 +20,14 @@ uint8_t ADC_Date(uint8_t i)
 	return ADCH;
 }
 
-uint16_t ADC_Average_Get(uint8_t pin)
+uint16_t ADC_Average_Get(uint8_t pin)		//8 samples are averaged
 {
 	uint8_t temp = 0;
 	for (uint8_t i = 0; i < 8; i++)
 	{
-		uint16_t j = ADC_Date(pin);
+		uint16_t j = ADC_Date(pin);	//Samples the sampled data in "j"
 		j /= 8;
-		temp += j;
+		temp += j;							
 	}
-	return temp;
+	return temp;				//The function returns the average of the 8 samples
 }
