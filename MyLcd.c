3ba0f8e7e2e0de326982a2e8c1ed7e7e5909be28
Miku311 <heidazl414@outlook.com>
2017-07-09 14:55:54 +0800

    Update MyLcd.c

--- MyLcd.c
+++ MyLcd.c
@@ -10,27 +10,30 @@
 /************************************************************************/
 /* ShuiXianHua and Display                                              */
 /************************************************************************/
-void my_display(uint8_t x, uint8_t y, uint32_t i)
+void my_display_3(uint8_t x, uint8_t y, uint32_t i)
 {
-	uint8_t display[3];
-	i = i * 255 / 255;
-	display[0] = i / 100 + 48;
-	display[1] = i % 100 / 10 + 48;
-	display[2] = i % 10 + 48;
-	LocateXY(x, y);
-	LCD_Wdat(display[0]);
+	uint8_t display[3];				//Create an array to hold each digit
+	i = i * map_value_3 / 255;			//In order to improve the speed of the calculation of "i = i / 255 * map_value_3;" optimized to "i = i * map_value_3 / 255;"
+	display[0] = i / 100 + 48;			//hundreds are stored in display[0]
+	display[1] = i % 100 / 10 + 48;			//Ten are stored in display[1]
+	display[2] = i % 10 + 48;			//Bit are stored in display[2]
+	LocateXY(x, y);					//Determine the coordinates displayed on 1602
+	LCD_Wdat(display[0]);				//1602 display
 	LCD_Wdat(display[1]);
 	LCD_Wdat(display[2]);
 }
 
-void Welcome(void)
+void my_display_4(uint8_t x, uint8_t y, uint32_t i)
 {
-	LocateXY(0,0);
-	LCD_Wdat(0x58);
-	LCD_Wdat(0xE0);
-	LCD_Wdat(0x79);
-	LCD_Wdat(0xE6);
-	LCD_Wdat(0xE3);
-	LCD_Wdat(0xDA);
-	LCD_Wdat(0xE3);
+	uint8_t display[4];				//Create an array to hold each digit
+	i = i * map_value_4 / 1023;			//In order to improve the speed of the calculation of "i = i / 1023 * map_value_4;" optimized to "i = i * map_value_4 / 1023;"
+	display[0] = i / 1000 + 48;			//Thousands of bits are stored in display[0]
+	display[1] = i % 1000 / 100 + 48;		//Hundreds are stored in display[1]
+	display[2] = i % 100 / 10 + 48;			//Ten are stored in display[2]
+	display[3] = i % 10 + 48;			//Bit are stored in display[3]
+	LocateXY(x, y);					//Determine the coordinates displayed on 1602
+	LCD_Wdat(display[0]);				//1602 display
+	LCD_Wdat(display[1]);
+	LCD_Wdat(display[2]);
+	LCD_Wdat(display[3]);
 }
