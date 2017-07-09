86afa7ec443c0ab0fbd04122805c61d3274d57c9
Miku311 <heidazl414@outlook.com>
2017-07-09 17:51:51 +0800

    Update ADC.h

--- ADC.h
+++ ADC.h
@@ -5,7 +5,6 @@
  *  Author: dusch
  */ 
 
-
 #ifndef ADC_H_
 #define ADC_H_
 
@@ -14,14 +13,18 @@
 uint8_t ADC_Date(uint8_t i);
 uint16_t ADC_Average_Get(uint8_t pin);
 
-typedef union _ADC_DATE
+
+
+/******* data processing *******/
+
+typedef union _ADC_DATE					//Defining a consortium is easier to break down the different parts of the data
 {
-	struct
+	struct						/A 16bit data into two 8bit data
 	{
-		unsigned low : 8;
+		unsigned low : 8;					
 		unsigned high : 8;
 	} half;
-	unsigned short all;
+	unsigned short all;				//Decomposed 16bit data
 } ADC_DATE;
 
 #endif /* ADC_H_ */
