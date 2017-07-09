3ef733b6b22104b3499aad5e3b266025ec52b2db
Miku311 <heidazl414@outlook.com>
2017-07-09 14:34:36 +0800

    Update ADC.h
    Annotations

--- ADC.h
+++ ADC.h
@@ -1,27 +1,30 @@
- /*
+/*
  * ADC.h
  *
  * Created: 2016/11/14 20:05:41
  *  Author: dusch
  */ 
 
-
 #ifndef ADC_H_
 #define ADC_H_
 
 #include "avr/io.h"
 
-unsigned int ADC_Date(unsigned char i);
+uint8_t ADC_Date(uint8_t i);
 uint16_t ADC_Average_Get(uint8_t pin);
 
-typedef union _ADC_DATE
+
+
+/******* data processing *******/
+
+typedef union _ADC_DATE				//Defining a consortium is easier to break down the different parts of the data
 {
-	struct
+	struct					//A 16bit data into two 8bit data
 	{
-		unsigned low : 8;
+		unsigned low : 8;					
 		unsigned high : 8;
 	} half;
-	unsigned short all;
+	unsigned short all;			//Decomposed 16bit data
 } ADC_DATE;
 
 #endif /* ADC_H_ */
