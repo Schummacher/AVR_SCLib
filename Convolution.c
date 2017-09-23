777f8f57d38774252037f968ee4a0d11df87de66
SJF <duschummacher@outlook.com>
2017-09-23 14:22:35 +0800

    Create Convolution.c

---
+++ Convolution.c
@@ -0,0 +1,12 @@
+//Author: Schummacher
+//Data: 2017-09-23
+
+
+#include "Convolution.h"
+
+void convolution(sequence *y, sequence *x, sequence *h)
+{
+	for (int i = 0; i < x->length; i++)
+		for (int j = 0; j < h->length; j++)
+			*(y->data + i + j) += *(x->data + i) * *(h->data + j);
+}
