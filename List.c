3c82ff1859d5a31bea70ee71e0dda226b490b616
SJF <duschummacher@outlook.com>
2019-02-19 17:33:59 +0800

    beat

--- List.c
+++ List.c
@@ -7,35 +7,72 @@
 
 #include "List.h"
 
-void List_Insert(ElementType data, list *L)
-{
-	list *tmp_cell = malloc(sizeof(list));
-	tmp_cell->num = data;
-	tmp_cell->next = L->next;
-	L->next = tmp_cell;
+void push(int dat, list *L){
+	// list *pl;
+	// pl = (list*)malloc(sizeof(list));
+	// pl->dat = dat;
+	// pl->next = L;
+	list_i *pl;
+	pl = (list_i*)malloc(sizeof(list_i));
+	pl->next = L->first;
+	pl->front = 0;
+	pl->dat = dat;
+	L->first = pl;
+	L->length++;
 }
 
-_Bool List_if_Empty(list *L)
-{
-	return L->next == NULL;
+int pop(list *L){
+	int dat;
+	list_i *pl;
+	dat = L->first->dat;
+	pl = L->first;
+	L->first = L->first->next;
+	L->first->front = 0;
+	L->length--;
+	free(pl);
+	return dat;
 }
 
-list *List_Find(ElementType data, list *L)
-{
-	list *P;
-	P = L->next;
-	while(P->num != data)
-		P = P->next;
-	return P;
+void creat(int dat, list *L){
+	list_i *pl;
+	pl = (list_i*)malloc(sizeof(list_i));
+	pl->dat = dat;
+	pl->next = 0;
+	pl->front = 0;
+	L->first = pl;
+	L->last = pl;
+	L->length = 1;
 }
 
-void
-List_Delete(ElementType data, list *L)
-{
-	list *P, *TmpCell;
 
-	P = List_Find(data, L);
-	TmpCell = P->next;
-	P->next = TmpCell->next;  /* Bypass deleted cell */
-	free( TmpCell );
+void append(int dat, list *L){
+	list_i *pl;
+	pl = (list_i*)malloc(sizeof(list_i));
+	pl->front = L->last;
+	pl->next = 0;
+	pl->dat = dat;
+	L->last = pl;
+	L->length++;
+}
+
+int out(list *L){
+	int dat;
+	list_i *pl;
+	dat = L->last->dat;
+	pl = L->last;
+	L->last = L->last->front;
+	L->last->next = 0;
+	L->length--;
+	free(pl);
+	return dat;
+}
+
+void clear(list *L){
+	list_i *pl;
+	while(L->length > 0){
+		pl = L->first;
+		L->first = L->first->next;
+		free(pl);
+		L->length--;
+	}	
 }
