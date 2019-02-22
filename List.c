c4096bcf9c47108c96118c393a74331f3064fdb2
SJF <duschummacher@outlook.com>
2019-02-22 10:42:48 +0800

    fix

--- List.c
+++ List.c
@@ -7,21 +7,18 @@
 
 #include "List.h"
 
-void push(int dat, list *L){
-	// list *pl;
-	// pl = (list*)malloc(sizeof(list));
-	// pl->dat = dat;
-	// pl->next = L;
+void push(int dat, list *L) {
 	list_i *pl;
 	pl = (list_i*)malloc(sizeof(list_i));
 	pl->next = L->first;
+	L->first->front = pl;
 	pl->front = 0;
 	pl->dat = dat;
 	L->first = pl;
 	L->length++;
 }
 
-int pop(list *L){
+int pop(list *L) {
 	int dat;
 	list_i *pl;
 	dat = L->first->dat;
@@ -33,7 +30,7 @@ int pop(list *L){
 	return dat;
 }
 
-void creat(int dat, list *L){
+void creat(int dat, list *L) {
 	list_i *pl;
 	pl = (list_i*)malloc(sizeof(list_i));
 	pl->dat = dat;
@@ -45,7 +42,7 @@ void creat(int dat, list *L){
 }
 
 
-void append(int dat, list *L){
+void append(int dat, list *L) {
 	list_i *pl;
 	pl = (list_i*)malloc(sizeof(list_i));
 	pl->front = L->last;
@@ -55,7 +52,7 @@ void append(int dat, list *L){
 	L->length++;
 }
 
-int out(list *L){
+int out(list *L) {
 	int dat;
 	list_i *pl;
 	dat = L->last->dat;
@@ -67,12 +64,67 @@ int out(list *L){
 	return dat;
 }
 
-void clear(list *L){
+void clear(list *L) {
 	list_i *pl;
-	while(L->length > 0){
+	while (L->length > 0) {
 		pl = L->first;
 		L->first = L->first->next;
 		free(pl);
 		L->length--;
-	}	
+	}
+}
+
+list *combian(list *A, list *B) {
+	list *C;
+	C = (list*)malloc(sizeof(list));
+	A->last->next = B->first;
+	B->first->front = A->last;
+	C->first = A->first;
+	C->last = B->last;
+	C->length = A->length + B->length;
+	free(A);
+	free(B);
+	return C;
+}
+
+void sweep(int *a, int *b) {
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+
+int find(int count, list *L, int face) {
+	list_i *pl;
+	int ans;
+	pl = (list_i*)malloc(sizeof(list_i));
+	if (count >= L->length) {
+		ans = 0;
+	}
+	else if (face == 0) {
+		pl->next = L->first;
+		if (count == 0) {
+			ans = pl->next->dat;
+		}
+		else {
+			for (int i = 0; i < count; i++) {
+				pl->next = pl->next->next;
+			}
+			ans = pl->next->dat;
+		}
+	}
+	else {
+		pl->front = L->last;
+		if (count == 0) {
+			ans = pl->front->dat;
+		}
+		else {
+			for (int i = 0; i < count; i++) {
+				pl->front = pl->front->front;
+			}
+			ans = pl->front->dat;
+		}
+	}
+	return ans;
 }
